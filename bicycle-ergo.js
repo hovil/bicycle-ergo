a0628ae2fb4e6ea55c388b26ee846143266f05d4
hovil <hovil@github.com>
2018-03-10 16:16:49 +1100

    Added seattube

--- bicycle-ergo.js
+++ bicycle-ergo.js
@@ -5,8 +5,11 @@ var chainstay_hz_length = 41;
 var chainstay_vt_length = 52;
 var wheelbase = 105;
 var bb_drop = 8;
-var stem_height =10;
-var stem_length = 12;
+var seat_height = 5;
+var handlebar_stemheight =10;
+var handlebar_stemlength = 12;
+var seat_stemheight = 10;
+var seat_stemoffset = 2;
 var crank_length = 16.5;
 var wheelsize = 31;
 
@@ -18,22 +21,8 @@ project.currentStyle = {
 var basex = 100;
 var basey = 100;
 
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
+//Anchor point
 var start = new Point(basex,basey);
-
-// Move to start and draw a line from there
-//path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-// path.lineTo(start + [ 100, -50 ]);
-// var path2 = new Path.Circle({
-//     center: view.center,
-//     radius: 30,
-//     strokeColor: 'black'
-//});
-
-
 
 var BB = new Path.Circle({
     center: start,
@@ -41,26 +30,32 @@ var BB = new Path.Circle({
     fillColor: 'black'
 });
 
+// Main triangle
+var seattube_end = new Point(start+[0,-seattube_length]);
 var seattube = new Path.Line(start,start + [0,-seattube_length]);
-
 var tt_end = new Point(seattube.lastSegment.point.x+toptube_length,seattube.lastSegment.point.y)
 var toptube = new Path.Line(seattube.lastSegment.point,tt_end);
-
 var ht_end = new Point(toptube.lastSegment.point.x,toptube.lastSegment.point.y+headtube_length-3);
 var headtube = new Path.Line(toptube.lastSegment.point,ht_end);
-
 var downtube = new Path.Line(start,headtube.lastSegment.point);
 
+// Seat
+var seat = new Point(seattube_end + [0, - seat_stemheight]);
+var seat_stem = new Path.Line(seattube.lastSegment.point,seat);
+
+// Rear chainstay
 var cs_end = new Point(start.x-chainstay_hz_length,start.y-bb_drop);
 var hz_chainstay = new Path.Line(start,cs_end);
 var vt_chainstay = new Path.Line(seattube.lastSegment.point,cs_end);
 
+// Wheels
 var rearwheel = new Path.Circle(cs_end,wheelsize);
 var frontwheel = new Path.Circle(cs_end + [wheelbase,0],wheelsize);
 
+// Front Fork
 var fork = new Path.Line(frontwheel.interiorPoint,ht_end);
 
-var stem = new Path.Line(tt_end,tt_end - [0,stem_height]);
-var stem_offset = new Path.Line(stem.lastSegment.point,stem.lastSegment.point +[stem_length,0]);
-
-var handlebars = new Path.Arc(stem_offset.lastSegment.point,stem_offset.lastSegment.point + [5,5],stem_offset.lastSegment.point + [-3,12]);
+// Handlebars
+var handlebar_stem = new Path.Line(tt_end,tt_end - [0,handlebar_stemheight]);
+var handlebar_stemoffset = new Path.Line(handlebar_stem.lastSegment.point - [2,0] ,handlebar_stem.lastSegment.point +[handlebar_stemlength,0]);
+var handlebars = new Path.Arc(handlebar_stemoffset.lastSegment.point,handlebar_stemoffset.lastSegment.point + [5,5], handlebar_stemoffset.lastSegment.point + [-3,12]);
